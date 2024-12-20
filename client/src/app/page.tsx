e07fe4d94273b0978ac1de1995277ce12c819b05
Prish20 <contact@christadriansanya.me>
2024-12-20 04:48:54 +0300

    Complete the landing page with framer-motion animations and add a simple footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
+
+import NonDashboardNavbar from '@/components/NonDashboardNavbar'
+
 import React from 'react'
+import Landing from '@/app/(nondashboard)/landing/page'
+import Footer from '@/components/Footer'
 
-const page = () => {
-  return (
-    <div>hi</div>
-  )
+
+export default function Home() {
+    return (
+        <div className="nondashboard-layout">
+            <NonDashboardNavbar />
+            <main className="nondashboard-layout__main">
+                <Landing />
+            </main>
+            <Footer />
+        </div>
+    )
 }
-
-export default page
