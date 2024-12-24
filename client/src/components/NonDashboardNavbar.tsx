b23724f88f8ae83e6bb721f69f33a5b486f2520e
Prish20 <contact@christadriansanya.me>
2024-12-24 03:26:29 +0300

    Complete user authentication functionality using Clerk.

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 import { Bell, BookOpen } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const NonDashboardNavbar = () => {
+    const { user } = useUser();
+    const userRole = user?.publicMetadata?.userType as "student" | "teacher";
     return (
         <nav className='nondashboard-navbar'>
             <div className="nondashboard-navbar__container">
@@ -28,6 +34,37 @@ const NonDashboardNavbar = () => {
                         <Bell className='nondashboard-navbar__notification-icon' size={18} />
                     </button>
                     {/* SIGN IN BUTTONS */}
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                                baseTheme: dark,
+                                elements: {
+                                    userButtonOuterIdentifier: "text-customgreys-dirtyGrey",
+                                    userButtonBox: "scale-90 sm:scale-100",
+                                    userButtonPopoverFooter: {
+                                        background: "#25262F",
+                                        padding: "0rem 2,5rem",
+                                        "& > div > div:nth-child(1)": {
+                                            background: "#25262F",
+                                        },
+                                    },
+                                },
+                            }}
+                            showName={true}
+                            userProfileMode="navigation"
+                            userProfileUrl={
+                                userRole === "teacher" ? "/teacher/profile" : "/user/profile"
+                            }
+                        />
+                    </SignedIn>
+                    <SignedOut>
+                        <Link href="/signin" className='nondashboard-navbar__auth-button--login'>
+                            Login
+                        </Link>
+                        <Link href="/signup" className='nondashboard-navbar__auth-button--signup'>
+                            Sign Up
+                        </Link>
+                    </SignedOut>
                 </div>
             </div>
         </nav>
